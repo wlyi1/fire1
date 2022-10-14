1f8c3ea5055a9491094ab8e5ac81d7ad2f7b0db5
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-14 14:11:17 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,5 @@
 import streamlit as st
-from google.cloud import firestore
-import json
-from google.oauth2 import service_account
 
-key_dict = json.loads(st.secrets["textkey"])
-creds = service_account.Credentials.from_service_account_info(key_dict)
-db = firestore.Client(credentials=creds, project="testrandom1")
-
-# Create a reference to the Google post.
-doc_ref = db.collection("users").document("waliy")
-
-# Then get the data at that reference.
-doc = doc_ref.get()
-
-# Let's see what we got!
-st.write("The id is: ", doc.id)
-st.write("The contents are: ", doc.to_dict())
+st.header('Hello 🌎!')
+if st.button('Balloons?'):
+    st.balloons()
