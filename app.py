9d6b1417a20a27e0ca941c3b9fef3deeb6a92528
wlyi1 <82428894+wlyi1@users.noreply.github.com>
2022-10-14 11:50:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from google.cloud import firestore
 import json
+from google.oauth2 import service_account
 
 key_dict = json.loads(st.secrets["textkey"])
 creds = service_account.Credentials.from_service_account_info(key_dict)
